090da20a862da9375e54bc6803baa1764156dd13
xiplus <huangxuanyuxiplus@gmail.com>
2020-01-02 10:52:00 +0800

    Record logged_in_users

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
-from db import DB
-import socket
-from config import SERVER_LISTEN, SERVER_PORT
 import json
+import socket
+
+from config import SERVER_LISTEN, SERVER_PORT
+from db import DB
+
 
 database = DB()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((SERVER_LISTEN, SERVER_PORT))
 print('Listening at {}'.format(sock.getsockname()))
+logged_in_users = []  # 紀錄已登入使用者
 while True:
     data, address = sock.recvfrom(1024)
     text = data.decode()
@@ -21,8 +24,15 @@ while True:
     if 'login' in data:
         try:
             userinfo = database.user_login(data['login']['user_id'], data['login']['password'])
-            response['ok'] = True
-            response['userinfo'] = userinfo
+            if userinfo:
+                if len(logged_in_users) == 0 or (len(logged_in_users) == 1 and logged_in_users[0][0] == userinfo[0]):
+                    logged_in_users.append(userinfo)
+                    response['ok'] = True
+                    response['userinfo'] = userinfo
+                else:
+                    response['ok'] = False
+            else:
+                response['ok'] = False
         except Exception:
             response['ok'] = False
 
